7d7bfb6646e09d17d19e0cbc40319343c4a49f0c
Thomas Deutschländer <thomas@deutschlaender.org>
2022-11-29 13:13:56 +0100

    Tag 2: Ende Vormittag

--- app/src/Controller/UserController.php
+++ app/src/Controller/UserController.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -26,5 +29,39 @@ class UserController extends AbstractController
         return $this->render('user/list.html.twig', [
             'users' => $users,
         ]);
+    }
+
+    #[Route(
+        path: '/user/add',
+        name: 'app.user.add',
+        methods: ['GET']
+    )]
+    public function add(): Response
+    {
+        return $this->render('user/add.html.twig');
+    }
+
+    #[Route(
+        path: '/user/submit',
+        name: 'app.user.submit',
+        methods: ['POST']
+    )]
+    public function submit(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        //Verarbeite POST
+        $email = $request->request->get('email');
+
+        $user = (new User())->setEmail($email);
+        //Initialisiere Model und befülle es mit Daten
+
+
+        //speicher das Model/Entity
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        //Wenn erfolgreich redirect an Liste
+
+        //Wenn nicht
+        return $this->redirectToRoute('app.user.add');
     }
 }
---
+++ app/src/Entity/User.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private int $id;
+
+    #[ORM\Column(length: 255)]
+    private string $email;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+        return $this;
+    }
+}
